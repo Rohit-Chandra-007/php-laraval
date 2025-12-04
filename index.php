a503d84afd8b3506e4651c94bee000e23ef6b694
Rohit Chandra <chandrarohit91@gmail.com>
2025-12-04 17:51:04 +0530

    database configuration done and also improved against sql injections

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
- require "helper/helper.php";
- require "router.php";
+require "helper/helper.php";
+require "Database.php";
+//  require "router.php";
 
+$config= require "config.php";
 
-?>
+// connect to the database
+$db=new Database($config['database']);
+
+$id=$_GET["id"];
+$query="select * from posts where id= :id";
+$post = $db->query($query, ['id'=>$id])->fetchAll();
+dd($post);
+
